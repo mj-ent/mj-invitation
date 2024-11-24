7979a5cd550a227973a0137ff27b475e8e6d0795
hellosamuel <samuelis419@gmail.com>
2024-11-24 18:30:09 +0900

    mata 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,19 @@ import { nanum_gothic } from './fonts'
 export const metadata: Metadata = {
   title: '사무엘 🤎 다정 결혼합니다',
   description: '2024년 10월 5일',
+  robots: { index: false, follow: false, nocache: true },
+  openGraph: {
+    title: '사무엘 🤎 다정 결혼합니다',
+    description: '2024년 10월 5일',
+    url: 'https://mj-invitation.vercel.app',
+    images: [
+      {
+        url: 'https://mj-invitation.vercel.app/images/main.jpg',
+        width: 800,
+        height: 600,
+      },
+    ]
+  }
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
